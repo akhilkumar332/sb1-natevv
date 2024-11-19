3528cba8ebf95979e31519d82c5c24b22e333ed0
Akhil Kumar Barri <akhilkumar332@gmail.com>
2024-11-19 16:10:08 +0100

    Bug Fixes in DonorLogin

--- src/pages/donor/DonorLogin.tsx
+++ src/pages/donor/DonorLogin.tsx
@@ -126,9 +126,6 @@ export function DonorLogin() {
     <div className="animate-pulse">
       <div className="h-8 bg-gray-300 rounded mb-4"></div>
       <div className="h-4 bg-gray-300 rounded mb-2"></div>
-      <div className="h-4 bg-gray-300 rounded mb-2"></div>
-      <div className="h-12 bg-gray-300 rounded mb-4"></div>
-      <div className="h-10 bg-gray-300 rounded mb-4"></div>
     </div>
   );
 
